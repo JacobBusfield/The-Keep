e5bb1da43f4b015496d568549a929c8827530921
Jacob.Busfield <Jacob.Busfield@simomics.com>
2019-10-04 17:11:03 +0100

    Better centering

--- src/config.js
+++ src/config.js
@@ -1,11 +1,17 @@
 import Boot from './scenes/boot'
 import Preload from './scenes/preload'
 import Game from './scenes/game'
+import {
+    Scale
+} from 'phaser'
 
 export default {
-    width: window.innerWidth,
-    height: window.innerHeight,
-    backgroundColor: 'rgb(0, 0, 0)',
-    scene: [ Boot, Preload, Game ]
+    width: 1500,
+    height: 900,
+    backgroundColor: 'rgba(0, 0, 0)',
+    scene: [Boot, Preload, Game],
+    scale: {
+        mode: Scale.FIT,
+        autoCenter: Scale.CENTER_BOTH
+    },
 }
-
