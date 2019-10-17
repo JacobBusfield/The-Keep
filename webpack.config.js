ef87d62b6ca82b6d7eb97630c09d2519a6dd8525
Jacob.Busfield <Jacob.Busfield@simomics.com>
2019-10-17 16:45:26 +0100

    mobile attempt 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,47 @@
-const path = require('path')
-const webpack = require('webpack')
+const path = require("path")
+const webpack = require("webpack")
 
 module.exports = env => {
     return {
         entry: {
-            app: './src/index.js',
-            vendor: ['phaser']
+            app: "./src/index.js",
+            vendor: ["phaser"]
         },
         output: {
-            filename: '[name].bundle.js',
+            filename: "[name].bundle.js",
             path: path.join(__dirname),
-            publicPath: "./"
+            publicPath: "./" //TODO: move this to ./images/ add change where images are packed to.
         },
-        mode: env && env.production ? 'production' : 'development',
+        mode: env && env.production ? "production" : "development",
         module: {
-            rules: [{
+            rules: [
+                {
                     test: /\.js$/,
                     exclude: /node_modules/,
                     use: {
-                        loader: 'babel-loader',
+                        loader: "babel-loader",
                         options: {
-                            presets: ['@babel/preset-env']
+                            presets: ["@babel/preset-env"]
                         }
                     }
                 },
                 {
                     test: /\.(png|svg|jpg|gif)$/,
-                    use: [
-                        'file-loader',
-                    ],
+                    use: ["file-loader"]
                 },
                 {
                     test: [/\.vert$/, /\.frag$/],
-                    use: 'raw-loader'
+                    use: "raw-loader"
                 }
             ]
         },
         devServer: {
-            contentBase: './'
+            contentBase: "./"
         },
         plugins: [
             new webpack.DefinePlugin({
-                'CANVAS_RENDERER': JSON.stringify(true),
-                'WEBGL_RENDERER': JSON.stringify(true)
+                CANVAS_RENDERER: JSON.stringify(true),
+                WEBGL_RENDERER: JSON.stringify(true)
             })
         ],
         optimization: {
@@ -52,8 +51,8 @@ module.exports = env => {
                     default: false,
                     commons: {
                         test: /[\\/]node_modules[\\/]/,
-                        name: 'vendor',
-                        chunks: 'all',
+                        name: "vendor",
+                        chunks: "all",
                         minChunks: 2
                     }
                 }
