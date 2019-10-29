7f8447a4ff24232e5a2c039658e633a1046fb969
Jacob.Busfield <Jacob.Busfield@simomics.com>
2019-10-29 14:48:26 +0000

    tile hitbox

--- src/tiles/tile.js
+++ src/tiles/tile.js
@@ -1,15 +1,29 @@
-import { GameObjects } from 'phaser'
+import {
+    GameObjects
+} from 'phaser'
 
 export default class Tile extends GameObjects.Sprite {
     constructor(scene, x, y, texture, frame) {
         super(scene, x, y, texture, frame)
+
+        this.setInteractive(
+                new Phaser.Geom.Polygon([0, 32, 32, 13, 64, 32, 32, 50, 0, 32]),
+                Phaser.Geom.Polygon.Contains
+            )
+            .on('pointerover', () => {
+                this.setTint("0x555555")
+            })
+            .on('pointerout', () => {
+                this.clearTint()
+            })
+            .on('pointerup', () => {})
     }
 
-    create(){}
+    create() {}
 
-    update(){}
+    update() {}
 
-    worldChange(neighbours){
+    worldChange(neighbours) {
         // lets state of a tile update using global tiles
     }
 }
